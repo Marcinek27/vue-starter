821f52851bbc11003f5247d4203aa1f08c3f6c61
Marcin <kubasiak@student.agh.edu.pl>
2021-06-08 20:55:10 +0200

    First reactivity

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,32 @@
 <template>
-  <div>
-		<h1>Twój e-mail to {{ email }} </h1>
+	<div>
+		<h1>Twój e-mail to {{ email }}</h1>
+
+		<input type="text" v-model="email">
+		<button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
+
 		<div v-if="email.length < 10">Ale masz krótki adres!</div>
 		<div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
 		<div v-else>Twój adres e-mail jest stanowczo za długi.</div>
-		
-		<input type="text" v-model="email">
-		<button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
-  </div>
+
+	</div>
 </template>
 
 <script>
 export default {
 	data() {
 		return {
-			email: 'kubasiak@student.agh.edu.pl',
-			password: ''
-		};
+		email: 'kubasiak@agh.edu.pl',
+		password: ''
+  		};
 	},
 	methods: {
-		alertMyEmail() {
-			alert(this.email);
-		}	
+  		alertMyEmail() {
+    		alert(this.email);
+  		}
 	}
 };
+
 </script>
 
 
