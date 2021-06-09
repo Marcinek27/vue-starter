04581a537b588ef25b71f184049a94a2fdac27e1
Marcin <kubasiak@student.agh.edu.pl>
2021-06-09 06:25:28 +0200

    Added milligram framework

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,36 @@
 <template>
 	<div>
-		<h1>Twój e-mail to {{ email }}</h1>
-
-		<input type="text" v-model="email">
-		<button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
-
-		<div v-if="email.length < 10">Ale masz krótki adres!</div>
-		<div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
-		<div v-else>Twój adres e-mail jest stanowczo za długi.</div>
-
+		<h1>Witaj w systemie zapisów na zajęcia</h1>
+		<div v-if="authenticatedEmail != ''">
+			<h4>Jesteś zalogowany jako {{authenticatedEmail}}</h4>
+			<a @click="logMeOut">Wyloguj</a>
+		</div>
+		<div v-else>
+			Wpisz login
+			<input type="text" v-model="email">
+			<button @click="logMeIn">Wchodzę</button>
+		</div>
 	</div>
 </template>
 
 <script>
+import "milligram"
+
 export default {
 	data() {
 		return {
-		email: 'kubasiak@agh.edu.pl',
-		password: ''
+			email: '',
+			authenticatedEmail: ''
   		};
 	},
 	methods: {
-  		alertMyEmail() {
-    		alert(this.email);
-  		}
+  		logMeIn() {
+			this.authenticatedEmail = this.email
+		},
+		logMeOut() {
+			this.authenticatedEmail = ''
+			this.email = ''
+		}
 	}
 };
 
