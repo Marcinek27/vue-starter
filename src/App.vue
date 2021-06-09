c794e56582bc398d4afb5379bea496c902b9e5bd
Marcin <kubasiak@student.agh.edu.pl>
2021-06-09 21:22:45 +0200

    Front end implemented

--- src/App.vue
+++ src/App.vue
@@ -2,36 +2,39 @@
 	<div>
 		<h1>Witaj w systemie zapisów na zajęcia</h1>
 		<div v-if="authenticatedEmail != ''">
+
+			<meeting-page></meeting-page>
+
 			<h4>Jesteś zalogowany jako {{authenticatedEmail}}</h4>
 			<a @click="logMeOut">Wyloguj</a>
 		</div>
 		<div v-else>
-			Wpisz login
-			<input type="text" v-model="email">
-			<button @click="logMeIn">Wchodzę</button>
+			<login-form @login="logMeIn($event)"></login-form>
 		</div>
 	</div>
 </template>
 
 <script>
 import "milligram"
+import LoginForm from "./LoginForm";
+import MeetingPage from "./meetings/MeetingPage"
 
 export default {
 	data() {
 		return {
-			email: '',
 			authenticatedEmail: ''
   		};
 	},
 	methods: {
-  		logMeIn() {
-			this.authenticatedEmail = this.email
+  		logMeIn(username) {
+			this.authenticatedEmail = username
 		},
 		logMeOut() {
 			this.authenticatedEmail = ''
 			this.email = ''
 		}
-	}
+	},
+	components: {LoginForm, MeetingPage}
 };
 
 </script>
